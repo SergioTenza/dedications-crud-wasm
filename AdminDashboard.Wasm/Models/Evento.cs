94c43b4801fb5bca563d9fac79e65aa5047d5016
Sergio Tenza <sergeix77@gmail.com>
2021-05-07 18:33:30 +0200

    Rework Rutas

---
+++ AdminDashboard.Wasm/Models/Evento.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AdminDashboard.Wasm.Models
+{
+    public class Evento
+    {
+        public string Id { get; set; }
+        public string Inicio { get; set; }
+        public string Duracion { get; set; }
+        public string Ejecucion { get; set; }
+        public string Estado { get; set; }
+        public string Logo { get; set; }
+        public string Skin { get; set; }
+        public string Tema { get; set; }
+        public string Texto { get; set; }
+        public string[] Urls { get; set; }
+        public string Machine { get; set; }
+        public string User { get; set; }
+        public string[] Mesas { get; set; }
+
+    }
+}
---
+++ AdminDashboard.Wasm/Models/Machine.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AdminDashboard.Wasm.Models
+{
+    public class Machine
+    {
+        public string Id { get; set; }
+        public string Name { get; set; }
+        public string PrivateId { get; set; }
+        public string Location { get; set; }
+        public User[] user { get; set; }
+        public Evento[] Tasks { get; set; }
+        public string  status { get; set; }
+    }
+}
